274ac44826e6fd542b135915de0c2e116362b7df
sanjaykumar2763 <sanjaykumar2763@gmail.com>
2020-07-24 19:21:54 +0530

    init exec

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const router = express.Router();
-
-const { spawn } = require("child_process");
+const util = require("util");
+const execFile = util.promisify(require("child_process").execFile);
+const exec = util.promisify(require("child_process").exec);
 
 router.get("/health", function (req, res, next) {
   res.json({ status: "running" });
 });
 
-router.get("/scripts/:scriptName", function (req, res, next) {
+router.get("/scripts/:scriptName", async (req, res, next) => {
   const { scriptName } = req.params;
+
+  console.log("scriptName: ", scriptName);
+  const { stdout } = await execFile("node", ["--version"]);
+  console.log("stdout: ", stdout);
+
+  console.log("res: ", await exec("pwd"));
+
   res.end();
 });
 
