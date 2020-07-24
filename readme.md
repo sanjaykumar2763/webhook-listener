2ca5a4c5b5a401ef149a0a69168ea864fdc1dba2
sanjaykumar2763 <sanjaykumar2763@gmail.com>
2020-07-24 20:11:55 +0530

    Setup base location

---
+++ readme.md
@@ -0,0 +1,9 @@
+# **Simple Linux webhook listener**
+
+Provides an API which accepts the script name as path param.
+
+It runs only shell scripts.
+
+## **Configuration**
+
+**WH_LISTENER_SCRIPTS_PATH**: Base location of the scripts. Set this value to the base location of the scripts. Else the default value would be **/home/\$USER/wh_scripts/**
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,10 @@ const util = require("util");
 const execFile = util.promisify(require("child_process").execFile);
 const exec = util.promisify(require("child_process").exec);
 
+const currentUser = process.env.USER;
+const webhookListenerScriptsLocation =
+  process.env.WH_LISTENER_SCRIPTS_PATH || `/home/${currentUser}/wh_scripts/`;
+
 router.get("/health", function (req, res, next) {
   res.json({ status: "running" });
 });
@@ -11,11 +15,19 @@ router.get("/health", function (req, res, next) {
 router.get("/scripts/:scriptName", async (req, res, next) => {
   const { scriptName } = req.params;
 
-  console.log("scriptName: ", scriptName);
-  const { stdout } = await execFile("node", ["--version"]);
-  console.log("stdout: ", stdout);
+  console.log("Request to execute script ", scriptName);
 
-  console.log("res: ", await exec("pwd"));
+  const scriptPath = webhookListenerScriptsLocation + scriptName;
+
+  console.log("Script path: ", scriptPath);
+
+  try {
+    const { stdout } = await execFile("sh", [scriptPath]);
+
+    console.log(`Response for executing ${scriptPath}: ${stdout}`);
+  } catch (e) {
+    console.error(`Error while running ${scriptPath}: `, e);
+  }
 
   res.end();
 });
